304611edb76fb8f5876dc8ac5b43b171fffacf94
Fadhil Muhammad <fadhil.setiawan@dkatalis.com>
2026-09-09 21:48:55 +0700

    :pencil2: binary search

--- src/clojure_noob/check.clj
+++ src/clojure_noob/check.clj
@@ -6,3 +6,21 @@
 scourge
 ;; lets make this into a map again
 (into {} scourge)
+
+;; build binary search based on clojure
+(defn binary-search
+  "return the index in O(log n)"
+  [v t]
+  (loop [left 0
+         right (dec (count v))]
+    (if (<= left right)
+      (let [x (+ left right)
+            m (quot x 2)
+            v-m (get v m)]
+        (cond
+          (= v-m t) m
+          (< v-m t) (recur (inc m) right)
+          :else (recur left (dec m))))
+      nil)))
+
+(binary-search (vec (range 230 16782)) 782)
